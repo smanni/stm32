db8a7faf107c300beb08199cc34e25f384bcab3a
smanni <stefano.manni@gmail.com>
2014-02-06 00:14:01 +0100

    Added the empty projects (just a couple of docs and a template main) AT45DBxx_ReadWrite for the Atmel DataFlash AT45DBxx

---
+++ workspace_metal/AT45DBxx_ReadWrite/src/main.c
@@ -0,0 +1,14 @@
+#include "stm32f10x.h"
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_spi.h"
+#include "misc.h"
+
+/*
+ * MAIN
+ */
+int main() {
+
+	/* Control should never come here */
+	while(1);
+}
